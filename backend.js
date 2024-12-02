49534a07f89e8093e35670c562fad28f32fbb9ae
andreinwald <andreinwald@gmail.com>
2024-12-02 08:30:23 +0100

    Added array of connected users

--- backend.js
+++ backend.js
@@ -1,29 +1,38 @@
 const http = require('node:http');
 const url = require('url');
 
-http.createServer(
-    (request, response) => {
-        let parsedUrl = url.parse(request.url)
-        if (parsedUrl.pathname === '/events') {
-            response.statusCode = 200;
-            eventsRoute(request, response, parsedUrl);
-        } else {
-            response.statusCode = 404;
-            response.end();
-        }
-    }).listen(3000, '127.0.0.1', () => console.log('Listening 127.0.0.1:3000'));
+let usersConnected = {};
 
-function eventsRoute(request, response, parsedUrl) {
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Cache-Control", "no-cache");
-    response.setHeader("connection", "keep-alive");
-    response.setHeader("Content-Type", "text/event-stream");
-
-    let query = new URLSearchParams(parsedUrl.search);
-    console.log('Connected user ' + query.get('user_id'));
-
-    setInterval(() => {
-        const data = JSON.stringify({date: new Date()});
-        response.write(`id: ${(new Date()).toLocaleTimeString()}\ndata: ${data}\n\n`);
-    }, 1000);
+function handleRequest(request, response) {
+    let parsedUrl = url.parse(request.url)
+    if (parsedUrl.pathname === '/events') {
+        response.statusCode = 200;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("connection", "keep-alive");
+        response.setHeader("Content-Type", "text/event-stream");
+        let query = new URLSearchParams(parsedUrl.search);
+        let userId = query.get('user_id');
+        console.log('Connected user ' + userId);
+        usersConnected[userId] = response;
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
 }
+
+function sendEvents() {
+    if (usersConnected.length === 0) {
+        return;
+    }
+    for (let userId in usersConnected) {
+        let response = usersConnected[userId];
+        let data = JSON.stringify({date: new Date()});
+        response.write(`id: ${(new Date()).toLocaleTimeString()}\ndata: ${data}\n\n`);
+    }
+}
+
+http.createServer(handleRequest)
+    .listen(3000, '127.0.0.1', () => console.log('Listening 127.0.0.1:3000'));
+
+setInterval(sendEvents, 1000);
